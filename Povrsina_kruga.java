2f25e5ba8ebe2098f12f2af94e284a118bde99f0
Maja Mujanovic <mujanovic.@maja@gmail.com>
2022-02-12 12:45:21 +0100

    Napravite program koji izracuna povrsinu kruga

---
+++ Povrsina_kruga.java
@@ -0,0 +1,12 @@
+public class Povrsina_kruga {
+    public static void main(String[] args) {
+
+        double radius = 33;
+        double area = Math.PI * (radius * radius);
+
+        System.out.println("The area of the circle is: " + area);
+
+
+
+    }
+}
